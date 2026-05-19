630aa076b7b44e97afc788a202f44d25727f49fd
HammadSadique786 <hammadeisa1122@gmail.com>
2026-05-19 11:38:42 +0500

    Operators are special symbols in Python that perform operations on variables and values.

---
+++ Oprators.py
@@ -0,0 +1,59 @@
+# Assignment operator
+
+
+a = 23
+b = 5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+
+# Comperison Operator
+
+a = 20
+b = 30
+
+print(a > b)
+print(a < b)
+print(a == b)
+print(a <= b)
+print(a >= b)
+print(a != b)
+
+# Assignment Operator 
+
+a = 50
+b = 20
+
+a += b # a = a + b
+print(a)
+
+a -= b # a = a - b
+print(a)
+a *= b # a = a * b
+print(a)
+
+a /= b # a = a / b
+print(a)
+
+a //= b # a = a // b
+print(a)
+
+a **= b # a = a ** b 
+print(a)
+
+a %= b # a =  a % b
+print(a)
+
+
+# Logical Operator
+
+a = 10
+b = 5
+
+print(a > 5 and b < 10 )
+print(a > 5 or b < 10)
+print(not(a < b))
