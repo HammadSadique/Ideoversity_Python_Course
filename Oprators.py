6b0fa8caa09d815a9a017e9e30a33dda42088316
HammadSadique786 <hammadeisa1122@gmail.com>
2026-05-21 02:10:53 +0500

    Add Python practice files

--- Oprators.py
+++ Oprators.py
@@ -57,3 +57,49 @@ b = 5
 print(a > 5 and b < 10 )
 print(a > 5 or b < 10)
 print(not(a < b))
+
+# Practical Examples
+
+age = 20
+print(age >= 18)
+
+
+# Using comparison operator
+can_drive = age >= 18
+print("Can drive:", can_drive)
+
+# Using logical operator 'or'
+# If AT LEAST ONE is True, the condition is True
+is_student = True
+has_coupon = False
+
+if is_student or has_coupon:
+    print("You get a discount!")
+else:
+    print("You pay full price.")
+
+# Check if a number is even
+num = 10
+if num % 2 == 0:
+    print("Is even:", num)
+else:
+    print("Is odd")
+
+# Check if a character is an alphabet
+score = 60
+has_attendence = True
+if score >= 60 and has_attendence:
+    print("You are Passed")
+else:
+    print("You are Failed")
+
+# --- LEAP YEAR CODE ---
+year = 2024
+
+# A year is a leap year if it is divisible by 4, but not by 100 (unless it's also divisible by 400)
+is_leap_year = (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+
+if is_leap_year:
+    print(year, "is a Leap Year!")
+else:
+    print(year, "is NOT a Leap Year.")
---
+++ Variable and Datatype.py
@@ -0,0 +1,46 @@
+# Variable
+x = 5
+y = "John"
+print(x)
+print(y)
+
+
+# Typecasting
+# Integer
+age = 18
+salary = 189000
+temperature_celsius = -15
+bank_balance = -200
+
+print(type(age))  # -> type function will return type of data stored in variable
+print(type(salary))
+print(type(temperature_celsius))
+print(type(bank_balance))
+
+
+# Float
+percentage_marks = 96.98
+price = 799.99
+net_profit = -434.67
+
+print(type(percentage_marks))
+print(type(price))
+print(type(net_profit))
+
+# Complex
+complex_number = 5 + 6j
+
+print(type(complex_number))
+
+# String
+name = "Hammad"
+full_name = 'Hammad Sadique'
+
+print(type(name))
+print(type(full_name))
+
+
+# Length of string
+fruit = "Mango"
+len1 = len(fruit)
+print("Mango is a", len1, "letter word.")     
