94ef4190cd0497dcfc198afc2b9ac391b71122c8
HammadSadique786 <hammadeisa1122@gmail.com>
2026-06-27 13:14:33 +0500

    Add 11-b-Streamlit.py

---
+++ 11-b-Streamlit.py
@@ -0,0 +1,15 @@
+from google import genai
+import streamlit as st
+
+client = genai.Client(api_key='API KEY')
+
+st.title("Talk to Agent")
+st.write("This app demonstrates a conversational agent.")
+
+user_input = st.text_input("Ask a question:")
+if st.button("Submit"):
+    with st.spinner("Agent is thinking..."):
+        response = client.models.generate_content(
+            model='gemini-flash-latest', contents=user_input
+        )
+    st.write(response.text)
